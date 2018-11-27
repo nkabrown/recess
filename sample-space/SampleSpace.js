4cbff4f074ef7287ca4a0ff6801d9fb9490cd4f1
Nathan Brown <nathan.brown@annalect.com>
2018-11-26 22:14:50 -0500

    Draw axes

--- sample-space/SampleSpace.js
+++ sample-space/SampleSpace.js
@@ -7,12 +7,11 @@ export default class SampleSpace {
   }
 
   init() {
-    console.log(this.data);
 
     // we will need to constrain our coordinate system to show axes and labels
     const margin = { top: 20, right: 20, bottom: 40, left: 40 },
           width = document.getElementsByClassName('sample-space')[0].clientWidth - 100 - margin.right - margin.left,
-          height = document.getElementsByClassName('sample-space')[0].clientHeight - 100 - margin.top - margin.bottom;
+          height = document.getElementsByClassName('sample-space')[0].clientWidth - 100 - margin.top - margin.bottom;
 
     // define a container and coordinate system for our visualization
     const graph = d3.select(this.mount).select('svg')
@@ -20,6 +19,34 @@ export default class SampleSpace {
        .attr('height', height + margin.top + margin.bottom)
       .append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    const faces = [1, 2, 3, 4, 5, 6];
+
+    // define scale, input domain and output range
+    const x = d3.scalePoint()
+       .padding(0.5)
+       .domain(faces)
+       .range([0, width]);
+
+    const y = d3.scalePoint()
+       .padding(0.5)
+       .domain(faces)
+       .range([height, 0]);
+
+    // functions that generate axes
+    const xAxis = d3.axisBottom(x),
+          yAxis = d3.axisLeft(y);
+
+    graph.append('g')
+       .attr('class', 'x-axis')
+       .attr('transform', `translate(0, ${height})`)
+       .call(xAxis);
+
+    graph.append('g')
+       .attr('class', 'y-axis')
+       .call(yAxis);
+
+   graph.data(this.data);
   }
 
   tabulate() {
