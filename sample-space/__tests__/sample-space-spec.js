8f15d9353dd22057dd162d480b81e4f4b836064d
Nathan Brown <nathan.brown@annalect.com>
2018-11-30 15:15:14 -0500

    Fix test that checks length of data set tabulated

--- sample-space/__tests__/sample-space-spec.js
+++ sample-space/__tests__/sample-space-spec.js
@@ -3,12 +3,12 @@ import SampleSpace from '../SampleSpace.js';
 
 const test = require('tape');
 
-test("data length is equal to n choose k", t => {
+test("data length is equal to the number of 2-tuples for a set of k objects (k²)", t => {
   const faces = [1, 2, 3, 4, 5, 6];
   const space = new SampleSpace('#mount');
   space.tabulate(faces);
   const count = space.data.reduce((acc, curr) => acc + curr.length, 0);
   t.plan(1);
-  t.equal(count, 6*6);
+  t.equal(count, 6**2);
   t.end();
 });
