ef269771a7a18ba7b3382125b11687041c780d19
Nathan <nathan.brown@annalect.com>
2019-04-09 15:50:17 -0400

    Pass in correct numbers for 2019

--- pursuit/src/index.js
+++ pursuit/src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 import SlopeGraph from './Slope.js';
 
-const data = [{income: 18000, status:'Pre-program'}, {income:95000, status:'Post-program'}];
+const data = [{income: 18000, status:'Pre-program'}, {income:85000, status:'Post-program'}];
 
-new SlopeGraph('#mount', data, {top:20, right:0, bottom:40, left:65}, 320, 250).init();
+new SlopeGraph('#mount', data, {top:30, right:0, bottom:40, left:65}, 320, 250).init();
