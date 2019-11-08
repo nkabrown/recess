a81365f79e145813d83cfb5ced09e8ee11d49f85
Nathan <nathan.brown@annalect.com>
2019-11-08 15:20:59 -0500

    Generate test data

--- gantt/js/index.js
+++ gantt/js/index.js
@@ -1,6 +1,27 @@
 'use strict';
 
-console.log('smoketest');
-
 import Gantt from './Gantt.js';
 import Table from './Table.js';
+
+// generate testing data
+const channels = ['Channel 1', 'Channel 2', 'Channel 3', 'Channel 4', 'Channel 5', 'Channel 6', 'Channel 7', 'Channel 8'];
+// generate one quarters worth of days
+const quarter =  d3.timeDays(new Date(2019, 6), new Date(2019, 8, 31));
+let start, lift;
+const data = channels.reduce((acc, curr) => (start = Math.floor(Math.random() * quarter.length) - 1,
+                                             acc.push({
+                                               channel: curr,
+                                               start: quarter[start],
+                                               end: quarter[start +
+                                                            Math.floor(Math.random() *
+                                                            (quarter.length - start - 1)) + 1],
+                                               metrics: {
+                                                 ROI: Math.floor(Math.random() * 100),
+                                                 avg: (Math.random() * 50).toFixed(2),
+                                                 lift: (lift = Math.floor(Math.random() * 10),
+                                                       lift === 0 ? lift : `+${lift}`),
+                                                 growth: (Math.random() * 100).toFixed(2)
+                                               }
+                                             }), acc), []);
+
+console.log(data);
